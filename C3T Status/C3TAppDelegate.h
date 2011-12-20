15d7db9fe40ddd72374f29ec0bcd5a2aaff245e8
gizzard <gizzard@giz.me>
2011-12-20 15:07:45 +0100

    added growl framework

--- C3T Status/C3TAppDelegate.h
+++ C3T Status/C3TAppDelegate.h
@@ -3,17 +3,19 @@
 //  C3T Status
 //
 //  Created by Oliver Leitzgen on 20.12.11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Chaos Computer Club Trier e. V. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
+#import <Growl/Growl.h>
 
-@interface C3TAppDelegate : NSObject <NSApplicationDelegate>
+@interface C3TAppDelegate : NSObject <NSApplicationDelegate, GrowlApplicationBridgeDelegate>
 
-@property (weak)    IBOutlet NSMenu    *statusMenu;
-@property (strong)  NSStatusItem       *statusItem;
-@property (strong)  NSImage            *statusImage;
-@property (strong)  NSImage            *statusHighlightImage;
+@property (weak)    IBOutlet NSMenu     *statusMenu;
+@property (strong)  NSStatusItem        *statusItem;
+@property (strong)  NSImage             *statusImage;
+@property (strong)  NSImage             *statusHighlightImage;
+@property (strong)  NSData              *clubLogo;
 
 -(IBAction)pushedItem:(id)sender;
 
--- C3T Status/C3TAppDelegate.m
+++ C3T Status/C3TAppDelegate.m
@@ -3,7 +3,7 @@
 //  C3T Status
 //
 //  Created by Oliver Leitzgen on 20.12.11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Chaos Computer Club Trier e. V. All rights reserved.
 //
 
 #import "C3TAppDelegate.h"
@@ -11,6 +11,7 @@
 @implementation C3TAppDelegate
 
 @synthesize statusMenu, statusItem, statusImage, statusHighlightImage;
+@synthesize clubLogo;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
@@ -19,19 +20,42 @@
 
 - (void) awakeFromNib 
 {
+    clubLogo = [NSImage imageNamed:@"c3t_logo.png"];
     statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength];
     
     statusImage = [NSImage imageNamed:@"led_gray.png"];
+    statusHighlightImage = [NSImage imageNamed:@"led_blue.png"];
     
     [statusItem setImage:statusImage];
     [statusItem setAlternateImage:statusHighlightImage];
     [statusItem setMenu:statusMenu];
     [statusItem setToolTip:@"C3T Status"];
+    [statusItem setHighlightMode:YES];
+    
+    [GrowlApplicationBridge setGrowlDelegate:self];  
 }
 
 -(IBAction)pushedItem:(id)sender
 {
     NSLog(@"hello");
+    [GrowlApplicationBridge notifyWithTitle:@"C3T Status"
+                                description:@"Es ist Club!"
+                            notificationName:@"Club is online"
+                                    iconData:clubLogo 
+                                    priority:0
+                                    isSticky:NO
+                                clickContext:@"clicked"];
+}
+
+- (void) growlNotificationWasClicked:(id)clickContext
+{
+    NSLog(@"%@", clickContext);
+    [[NSWorkspace sharedWorkspace] openURL: [NSURL URLWithString: @"http://c3t.de/club/?status"]];
+}
+
+- (void)growlNotificationTimedOut:(id)clickContext
+{
+    NSLog(@"timeout");
 }
 
 @end
